5634ca0135607b2f2cac469f89080199ef809b8e
Christian Albrecht <albchr@gmail.com>
2023-01-31 19:39:53 +0100

    Game Manager added

--- Assets/Scripts/Characters/PlayerStats.cs
+++ Assets/Scripts/Characters/PlayerStats.cs
@@ -7,13 +7,13 @@ public class PlayerStats : MonoBehaviour
     [SerializeField] string PlayerName;
 
     [SerializeField] int maxLevel = 20;
-    [SerializeField] int playerLevel;
+    [SerializeField] int playerLevel = 1;
     [SerializeField] int currentXP;
 
     [SerializeField] int maxHP = 100;
     [SerializeField] int currentHP;
 
-    [SerializeField] int[] xpForEachLevel;
+    [SerializeField] int[] xpForNextLevel;
     [SerializeField] int baseLevelXP = 100;
 
     [SerializeField] int maxMana = 30;
@@ -25,13 +25,13 @@ public class PlayerStats : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        xpForEachLevel = new int[maxLevel];
-        xpForEachLevel[1] = baseLevelXP;
+        xpForNextLevel = new int[maxLevel];
+        xpForNextLevel[1] = baseLevelXP;
 
-        for (int i = 2; i < xpForEachLevel.Length; i++)
+        for (int i = 2; i < xpForNextLevel.Length; i++)
         {
             //print("We are at: " + i);
-            xpForEachLevel [i] = baseLevelXP *i;
+            xpForNextLevel [i] = (int) (0.02f*i*i*i + 3.06f*i*i + 105.6f*i);
         }
 
     }
@@ -39,6 +39,39 @@ public class PlayerStats : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            AddXP(100);
+        }
         
+    }
+
+    public void AddXP(int amountOfXP)
+    {
+        currentXP += amountOfXP;
+        Debug.Log(currentXP);
+        if(currentXP > xpForNextLevel[playerLevel])
+        {
+            currentXP -= xpForNextLevel[playerLevel];
+            playerLevel++;
+
+            if (playerLevel %2 == 0)
+            {
+                dexterity++;
+            }
+            else
+            {
+                defence++;
+            }
+
+            maxHP = Mathf.FloorToInt(maxHP * 1.18f);
+            currentHP = maxHP;
+
+            maxMana = Mathf.FloorToInt(maxMana * 1.06f);
+            currentMana = maxMana;
+
+
+
+        }
     }
 }
