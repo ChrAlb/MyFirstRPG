9a5b9f5ff96303c24e7877fe7c22a45c63baa3e1
Christian Albrecht <albchr@gmail.com>
2023-02-15 21:31:57 +0100

    stats panel finished

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -9,7 +9,8 @@ public class MenuManager : MonoBehaviour
 {
     [SerializeField] Image imageToFade;
     [SerializeField] GameObject menu;
-    
+
+    [SerializeField] GameObject[] statsButtons;
 
     public static MenuManager Instance;
 
@@ -19,28 +20,27 @@ public class MenuManager : MonoBehaviour
     [SerializeField] Image[] characterImage;
     [SerializeField] GameObject[] characterPanel;
 
-    public void QuitGame()
+    [SerializeField] TextMeshProUGUI statName, statHP, statMana, statDex, statDef;
+    [SerializeField] Image characterStatImage;
+ 
+    private void Start()
     {
-        Application.Quit(); 
-    }
-    
-    public void FadeImage()
-    {
-        imageToFade.GetComponent<Animator>().SetTrigger("StartFading");
+
+        Instance = this;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
-            
-            
+
+
             if (menu.activeInHierarchy)
             {
                 //UpdateStats(); // Im Tutorial hier. Beim ersten Aufruf kommen aber Stats nicht!
                 menu.SetActive(false);
                 GameManager.instance.gameMenuOpened = false;
-                
+
             }
             else
             {
@@ -53,6 +53,7 @@ public class MenuManager : MonoBehaviour
 
     }
 
+   
     public void UpdateStats()
     {
         playerStats = GameManager.instance.GetPlayerStats();
@@ -68,11 +69,47 @@ public class MenuManager : MonoBehaviour
 
             characterImage[i].sprite = playerStats[i].characterImage;
 
+            //Bei Szenenwechsel von 2 zu 1 geht das nicht mehr?
             xpText[i].text = playerStats[i].currentXP.ToString() + "/" + playerStats[i].xpForNextLevel[playerStats[i].playerLevel];
             xpSlider[i].maxValue = playerStats[i].xpForNextLevel[playerStats[i].playerLevel];
             xpSlider[i].value = playerStats[i].currentXP;
         }
     }
 
-    
+    public void StatsMenu()
+    {
+        for (int i = 0; i< playerStats.Length; i++)
+        {
+            statsButtons[i].SetActive(true);
+
+            statsButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = playerStats[i].PlayerName;
+        }
+
+        StatsMenuUpdate(0);
+    }
+
+    public void StatsMenuUpdate(int playerSelectedNumber)
+    {
+        PlayerStats playerSelected = playerStats[playerSelectedNumber];
+
+        statName.text = playerSelected.PlayerName;
+        statHP.text = playerSelected.currentHP.ToString() + "/" + playerSelected.maxHP;
+        statMana.text = playerSelected.currentMana.ToString() + "/" + playerSelected.maxMana;
+        statMana.text = playerSelected.currentMana.ToString() + "/" + playerSelected.maxMana;
+
+        statDex.text = playerSelected.dexterity.ToString();
+        statDef.text = playerSelected.defence.ToString();
+
+        characterStatImage.sprite = playerSelected.characterImage;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void FadeImage()
+    {
+        imageToFade.GetComponent<Animator>().SetTrigger("StartFading");
+    }
 }
