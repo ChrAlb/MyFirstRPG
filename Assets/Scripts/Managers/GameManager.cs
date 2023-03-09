33cfce7b6016db8feb7a149495e67bb593772604
Christian Albrecht <albchr@gmail.com>
2023-03-09 20:43:41 +0100

    Saving PlayerStats done

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -69,6 +69,38 @@ public class GameManager : MonoBehaviour
         PlayerPrefs.SetFloat("Player_Pos_Y", Player.instance.transform.position.y);
         PlayerPrefs.SetFloat("Player_Pos_Z", Player.instance.transform.position.z);
 
+        for  (int i = 0; i< playerStats.Length; i++)
+        {
+            if (playerStats[i].gameObject.activeInHierarchy)
+            {
+                PlayerPrefs.SetInt("Player_" + playerStats[i].PlayerName + "_active",1);
+            }
+            else
+            {
+                PlayerPrefs.SetInt("Player_" + playerStats[i].PlayerName + "_active", 0);
+            }
+
+            PlayerPrefs.SetInt("Player_" + playerStats[i].PlayerName + "_Level", playerStats[i].playerLevel);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].PlayerName + "_CurrentXP", playerStats[i].currentXP);
+
+            PlayerPrefs.SetInt("Player_" + playerStats[i].PlayerName + "_MaxHP", playerStats[i].maxHP);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].PlayerName + "_CurrentHP", playerStats[i].currentHP);
+
+            PlayerPrefs.SetInt("Player_" + playerStats[i].PlayerName + "_MaxMana", playerStats[i].maxMana);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].PlayerName + "_CurrentMana", playerStats[i].currentMana);
+
+            PlayerPrefs.SetInt("Player_" + playerStats[i].PlayerName + "_Dexterity", playerStats[i].dexterity);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].PlayerName + "_Defence", playerStats[i].defence);
+
+            PlayerPrefs.SetString("Player_" + playerStats[i].PlayerName + "_EquipedWeapon", playerStats[i].equippedWeaponName);
+            PlayerPrefs.SetString("Player_" + playerStats[i].PlayerName + "_EquipedArmor", playerStats[i].equippedArmorName);
+
+            PlayerPrefs.SetInt("Player_" + playerStats[i].PlayerName + "_WeaponPower", playerStats[i].weaponPower);
+            PlayerPrefs.SetInt("Player_" + playerStats[i].PlayerName + "_ArmorDefence", playerStats[i].armorDefence);
+
+        }
+
+
     }
 
     public void LoadData()
