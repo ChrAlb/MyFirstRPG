b0a24c6e18c97c8a15ee96ad049059032ca3c0a3
Christian Albrecht <albchr@gmail.com>
2023-03-25 15:55:48 +0100

    Restart mit Dungeon

---
+++ Assets/Scripts/Battle Systems/BattleManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    
+    public static BattleManager instance;
+    private bool isBattleActive;
+
+    [SerializeField] GameObject battleScene;
+    [SerializeField] List<BattleCharacters> activeCharacters = new List<BattleCharacters>();
+
+    [SerializeField] Transform[] playerPositions, enemyPositions;
+
+    [SerializeField] BattleCharacters[] playerPrefabs, enemiesPrefabs;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this; 
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            StartBattle(new string[] {"Maga Master","Warlock" });
+        }
+    }
+
+    public void StartBattle(string[] enemiesToSpawn)
+    {
+        SettingUpBattle();
+
+        for(int i = 0; i < GameManager.instance.GetPlayerStats().Length; i++)
+        {
+            
+
+            if (GameManager.instance.GetPlayerStats()[i].gameObject.activeInHierarchy)
+            {
+               
+                for (int j = 0; j< playerPrefabs.Length; j++)
+                {
+                    Debug.Log("bevor" + playerPrefabs[j].characterName);
+                    if (playerPrefabs[j].characterName == GameManager.instance.GetPlayerStats()[i].PlayerName)
+                    {
+                        Debug.Log(GameManager.instance.GetPlayerStats()[i].PlayerName);
+
+                        BattleCharacters newPlayer = Instantiate(
+                            playerPrefabs[j],
+                            playerPositions[i].position,
+                            playerPositions[i].rotation,
+                            playerPositions[i]
+                            );
+
+                        activeCharacters.Add(newPlayer);
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void SettingUpBattle()
+    {
+        if (!isBattleActive)
+        {
+            isBattleActive = true;
+            GameManager.instance.battleIsActive = true;
+
+            transform.position = new Vector3(
+                Camera.main.transform.position.x,
+                Camera.main.transform.position.y,
+                transform.position.z);
+
+            battleScene.SetActive(true);
+        }
+    }
+}
