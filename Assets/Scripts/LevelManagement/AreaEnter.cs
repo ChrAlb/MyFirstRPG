3a53b6e6d39e2b1d5ecaf41c302003456abd7055
Christian Albrecht <albchr@gmail.com>
2023-05-06 17:11:47 +0200

    Working on updating items by changing scene

--- Assets/Scripts/LevelManagement/AreaEnter.cs
+++ Assets/Scripts/LevelManagement/AreaEnter.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AreaEnter : MonoBehaviour
 {
+    
     public string transitionAreaName;
+    private string SceneName;
 
     // Start is called before the first frame update
     void Start()
@@ -20,4 +23,39 @@ public class AreaEnter : MonoBehaviour
     {
         
     }
+
+    private void LoadActiveItemsinScene()
+    {
+        if (PlayerPrefs.HasKey("Szene_" + SceneManager.GetActiveScene().buildIndex))
+        {
+            SceneName = PlayerPrefs.GetString("Szene_" + SceneManager.GetActiveScene().buildIndex);
+            
+
+        }
+       
+    }
 }
+
+/*
+private void SaveActiveItemsinScene()
+{
+    PlayerPrefs.SetString("Szene_" + SceneManager.GetActiveScene().buildIndex, SceneManager.GetActiveScene().name);
+
+    ActiveItemsinScene = GameObject.FindGameObjectsWithTag("Item");
+    for (int i = 0; i < ActiveItemsinScene.Length; i++)
+    {
+        if (ActiveItemsinScene[i].activeInHierarchy)
+        {
+            PlayerPrefs.SetString("Item_" + ActiveItemsinScene[i], ActiveItemsinScene[i].name);
+
+            PlayerPrefs.SetFloat("Item_Pos_X" + ActiveItemsinScene[i], ActiveItemsinScene[i].transform.position.x);
+            PlayerPrefs.SetFloat("Item_Pos_Y" + ActiveItemsinScene[i], ActiveItemsinScene[i].transform.position.y);
+            PlayerPrefs.SetFloat("Item_Pos_Z" + ActiveItemsinScene[i], ActiveItemsinScene[i].transform.position.z);
+            print(ActiveItemsinScene[i].name);
+            print(ActiveItemsinScene[i].transform.position.x);
+            print(ActiveItemsinScene[i].transform.position.y);
+            print(ActiveItemsinScene[i].transform.position.z);
+        }
+    }
+
+}*/
--- Assets/Scripts/LevelManagement/AreaExit.cs
+++ Assets/Scripts/LevelManagement/AreaExit.cs
@@ -9,6 +9,8 @@ public class AreaExit : MonoBehaviour
     [SerializeField] string transitionAreaName;
     [SerializeField] AreaEnter theAreaEnter;
 
+    private GameObject[] ActiveItemsinScene; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,8 +34,32 @@ public class AreaExit : MonoBehaviour
         
     }
 
+    private void SaveActiveItemsinScene()
+    {
+        PlayerPrefs.SetString("Szene_" + SceneManager.GetActiveScene().buildIndex, SceneManager.GetActiveScene().name);
+
+        ActiveItemsinScene = GameObject.FindGameObjectsWithTag("Item");
+        for (int i = 0; i < ActiveItemsinScene.Length; i++)
+        {
+            if (ActiveItemsinScene[i].activeInHierarchy)
+            {
+                PlayerPrefs.SetString("Item_" + ActiveItemsinScene[i], ActiveItemsinScene[i].name);
+
+                PlayerPrefs.SetFloat("Item_Pos_X" + ActiveItemsinScene[i], ActiveItemsinScene[i].transform.position.x);
+                PlayerPrefs.SetFloat("Item_Pos_Y" + ActiveItemsinScene[i], ActiveItemsinScene[i].transform.position.y);
+                PlayerPrefs.SetFloat("Item_Pos_Z" + ActiveItemsinScene[i], ActiveItemsinScene[i].transform.position.z);
+                print (ActiveItemsinScene[i].name);
+                print (ActiveItemsinScene[i].transform.position.x);
+                print (ActiveItemsinScene[i].transform.position.y);
+                print (ActiveItemsinScene[i].transform.position.z);
+            }
+        }
+        
+    }
+
     IEnumerator LoadSceneCoroutine()
     {
+        SaveActiveItemsinScene();
         yield return new WaitForSeconds(0.7f);
         SceneManager.LoadScene(sceneToLoad);
     }
